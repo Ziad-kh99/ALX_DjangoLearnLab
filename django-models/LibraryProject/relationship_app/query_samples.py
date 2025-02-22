fcdba72bc615e23c7b9c267330a017de33f366ca
Ziad Khaled <ziadkh99@hotmail.com>
2025-02-22 22:24:33 +0200

    Create relationship_app and some models and do simple quiries on these models

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,29 @@
+from .models import Book, Author, Library, Librarian
+
+
+
+#> Query all books by a specific author:
+author = Author.objects.filter(name = 'Ziad Khaled').first()
+
+if author:
+    books = Book.objects.filter(author = author)
+else:
+    print('No author found with that name.')
+
+
+
+
+#> List all books in a library:
+library = Library.objects.filter(name = 'Lib Name').first()
+
+if library:
+    books = library.books.all()
+
+
+
+#> Retrieve the librarian for a library:
+library = Library.objects.filter(name = 'Lib Name').first()
+
+if library:
+    librarian = Librarian.objects.filter(library = library)
+
