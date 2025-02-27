e0b3f49cf3f94db52d3f11992c6b748fb21d27f9
Ziad Khaled <ziadkh99@hotmail.com>
2025-02-27 21:32:25 +0200

    Implement auth functionalities

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books
-from .views import LibraryDetailView
+from .views import LibraryDetailView, SignUpView
+
 
 
 urlpatterns = [
     path('listbooks/', list_books), 
-    path('librarydetails/', LibraryDetailView),
+    path('librarydetails/', LibraryDetailView.as_view(template_name='relationship_app/library_detail.html')),
+    path('register/', SignUpView.as_view(template_name='relationship_app/register.html'), name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 from .models import Book
 from .models import Library
 
@@ -17,3 +20,9 @@ class LibraryDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         library = Library.objects.filter(name = self.library_name)
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
