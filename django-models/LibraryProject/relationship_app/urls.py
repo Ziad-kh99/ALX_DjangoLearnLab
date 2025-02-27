57d10ebbce2621c49a0119e064066cbb4b62b13b
Ziad Khaled <ziadkh99@hotmail.com>
2025-02-27 21:43:28 +0200

    define logout template

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books
 from .views import LibraryDetailView, SignUpView
-
+# views.register
 
 
 urlpatterns = [
@@ -10,5 +10,5 @@ urlpatterns = [
     path('librarydetails/', LibraryDetailView.as_view(template_name='relationship_app/library_detail.html')),
     path('register/', SignUpView.as_view(template_name='relationship_app/register.html'), name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -27,3 +27,6 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
+
+class register:
+    pass
