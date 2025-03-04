7ed5fe0a8466d2b4dd287e5357fbfea340fcde07
Ziad Khaled <ziadkh99@hotmail.com>
2025-03-04 14:49:57 +0200

    Integrate the Custom User Model into Admin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'data_of_birth', 'profile_photo')
+
+admin.site.register(CustomUser, CustomUserAdmin)
