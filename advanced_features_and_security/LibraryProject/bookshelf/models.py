503d8506f3dc460aca1680bf19698ebfac8187cd
Ziad Khaled <ziadkh99@hotmail.com>
2025-03-04 14:43:20 +0200

    move custom user manager to models file

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,8 +1,24 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField()
     profile_photo = models.ImageField()
+
+from django.contrib.auth.models import BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password, date_of_birth, profile_photo):
+        user = self.model(username=username, date_of_birth=date_of_birth, profile_photo=profile_photo)
+        user = set_password(password)
+        user.save()
+        return user
     
+    def create_superuser(self, username, password, date_of_birth, profile_photo):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fileds.setdefault('is_active', True)
+
+        return self.create_user(username, password, date_of_birth, profile_photo)
