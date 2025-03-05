0eb7a9907183d5d0324a308306ccff52c011c141
Ziad Khaled <ziadkh99@hotmail.com>
2025-03-05 22:01:15 +0200

    add book_list view and apply can_list perm on it

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import permission_required
 
+@permission_required('bookshelf.can_view')
+def book_list(request):
+    return HttpResponse('List all books')
+
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
     return HttpResponse('You can create book')
