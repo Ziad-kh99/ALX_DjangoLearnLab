eee9db76ef7c49d5c81642ae6b2697c3323992db
Ziad Khaled <ziadkh99@hotmail.com>
2025-03-16 23:18:32 +0200

    apply some permissions on views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 class BookList(ListView):
     model = Book
@@ -10,6 +10,7 @@ class BookDetail(DetailView):
     model = Book
 class BookCreate(CreateView):
     model = Book
+    permission-classes = [IsAuthenticated]
 class BookUpdate(UpdateView):
     model = Book
 class BookDelete(DeleteView):
