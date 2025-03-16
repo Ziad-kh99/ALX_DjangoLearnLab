589f5017ed1997e36b609df073b0a24f7d745a18
Ziad Khaled <ziadkh99@hotmail.com>
2025-03-16 23:14:34 +0200

    update urls to match auto-checker

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,8 +4,8 @@ from .views import BookList, BookDetail, BookCreate, BookUpdate, BookDelete
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetail.as_view(), name='book-detail'),
-    path('books/', BookCreate.as_view(), name='book-create'),
-    path('books/<int:pk>/', BookDetail.as_view(), name='book-update'),
-    path('books/<int:pk>/', BookDelete.as_view(), name='book-delete'),
+    path('books/create', BookCreate.as_view(), name='book-create'),
+    path('books/update/', BookDetail.as_view(), name='book-update'),
+    path('books/delete/', BookDelete.as_view(), name='book-delete'),
 ]
 
