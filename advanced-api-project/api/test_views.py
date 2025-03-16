b95df8423708e94c1253dc2ef932020ec2e3412f
Ziad Khaled <ziadkh99@hotmail.com>
2025-03-17 01:03:22 +0200

    write a unit test for book list endpoint

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,6 +1,21 @@
 from rest_framework.test import APIRequestFactory
+from rest_framework import status
+from django.test import TestCase
+from .models import Book
+from .views import BookListView
 
-class APITestCase(APIRequestFactory):
-    def test1(self):
-        pass
+
+class BookAPITestCase(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.book = Book.objects.create(title='Book1', publication_year=1958)
+
+    def test_list_books(self):
+        view = BookListView.as_view()({'get': 'retrive'})
+        request = self.factory.get(f'/books/{self.book.id}/')
+        response = view(request, pk=self.book.id)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['title'], '1958')
+
         
