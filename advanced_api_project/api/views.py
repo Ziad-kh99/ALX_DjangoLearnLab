0d8f7177ddc24f649a3cdc7eccd98b49252f38ee
Ziad Khaled <ziadkh99@hotmail.com>
2025-03-16 23:07:55 +0200

    Create custom views and config its urls

--- advanced_api_project/api/views.py
+++ advanced_api_project/api/views.py
@@ -2,26 +2,17 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
-class ListView (generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-class DetialView (generics.RetrieveAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-class CreateView (generics.CreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-class UpdateView (generics.UpdateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-class DeleteView(generics.DestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
+class BookList(ListView):
+    model = Book
+class BookDetail(DetailView):
+    model = Book
+class BookCreate(CreateView):
+    model = Book
+class BookUpdate(UpdateView):
+    model = Book
+class BookDelete(DeleteView):
+    model = Book
 
