bf052c9c3651234d9901234424b4f1adacc65b9f
Ziad Khaled <ziadkh99@hotmail.com>
2025-03-19 00:02:09 +0200

    Implement Auth features: Register, Longin, and Logout

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from blog import views as user_views
+
+
+urlpatterns = [
+    path('', user_views.home, name='home'),
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-# Create your views here.
+def home(request):
+    return render(request, 'blog/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to login.')
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'blog/register.html', context)
+
