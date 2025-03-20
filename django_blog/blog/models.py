1053241ad7f8092a0c7edb2b79947258b5f1ce7f
Ziad Khaled <ziadkh99@hotmail.com>
2025-03-20 21:34:41 +0200

    Implement profile management

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -8,4 +8,10 @@ class Post(models.Model):
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
+    def __str__(self):
+        return f'{self.user.username} Profile.'
+    
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, ProfileUpdateform, UserUpdateForm
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'blog/home.html')
@@ -22,3 +23,27 @@ def register(request):
 
     return render(request, 'blog/register.html', context)
 
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateform(request.POST, request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'Your account has been updated.')
+            return redirect('profile')
+        
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateform(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+
+    return render(request, 'blog/profile.html', context)
